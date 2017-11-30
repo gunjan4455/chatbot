c96560c868bc949fd6dd65f579ee38a795bd4b84
shobhit <shob@saxobank.com>
2017-11-30 15:40:16 +0530

    input form separated

--- app/components/Home/Home.jsx
+++ app/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import ChatBot from 'react-simple-chatbot';
 import {renderBooksList, emailValidator} from "../../utility";
 import io from 'socket.io-client';
+import InputForm from "./InputForm";
 
 const socket = io('http://localhost:9000');
 
@@ -157,99 +158,5 @@ Home.propTypes = {
 };
 
 export default Home;
-
-
-export class InputForm extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.details = {
-            userid: '',
-            password: '',
-            email: ''
-        };
-        this.state = {
-            showForm : true
-        }
-    }
-
-    onChange = (type, evt) => {
-        switch (type) {
-            case 'userid':
-                this.details.userid = Number(evt.target.value);
-                break;
-            case 'password':
-                this.details.password = evt.target.value;
-                break;
-            case 'email':
-                this.details.email = evt.target.value;
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    onSubmit = (evt) => {
-        evt.preventDefault();
-        this.props.addUser(this.details);
-    }
-
-    componentWillReceiveProps(nextProps, prv) {
-        if(!_.isEmpty(nextProps.user) && !_.isEmpty(nextProps.user.isAdmin))
-            nextProps.history.push('/admin');
-        else {
-            this.setState({showForm : false});
-            nextProps.triggerNextStep();
-            //nextProps.createConnection();
-        }
-    }
-
-    render() {
-        return (
-            <div className="container">
-                {this.state.showForm ?
-                    <div>
-                        { this.props.option ? (
-                            <form className="form-horizontal" onSubmit={this.onSubmit}>
-                                <div style={{"marginBottom": "25px"}} className="input-group">
-                                    <span className="input-group-addon"><i
-                                        className="glyphicon glyphicon-user"></i></span>
-                                    <input className="form-control" defaultValue="" placeholder="userid"
-                                           onChange={this.onChange.bind(this,'userid')}/>
-                                </div>
-                                <div style={{"marginBottom": "25px"}} className="input-group">
-                                    <span className="input-group-addon"><i
-                                        className="glyphicon glyphicon-lock"></i></span>
-                                    <input type="password" className="form-control" defaultValue=''
-                                           placeholder="password"
-                                           onChange={this.onChange.bind(this,'password')}/>
-                                </div>
-                                <div style={{"marginTop":"10px"}} className="form-group">
-                                    <div className="col-sm-12 controls">
-                                        <button type="submit" className="btn btn-success">Submit</button>
-                                    </div>
-                                </div>
-                            </form> ) : (
-                            <form className="form-horizontal" onSubmit={this.onSubmit}>
-                                <div style={{"marginBottom": "25px"}} className="input-group">
-                                    <span className="input-group-addon"><i
-                                        className="glyphicon glyphicon-user"></i></span>
-                                    <input className="form-control" defaultValue="" placeholder="email"
-                                           onChange={this.onChange.bind(this,'email')}/>
-                                </div>
-                                <div style={{"marginTop":"10px"}} className="form-group">
-                                    <div className="col-sm-12 controls">
-                                        <button type="submit" className="btn btn-success">Submit</button>
-                                    </div>
-                                </div>
-                            </form>)
-                        }
-                        </div>
-                     : <div>Successfully registered</div> }
-            </div>
-        );
-    }
-}
-
 
 
